efbc394e5acdb2fe8b8577dd93806c54db068199
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2025-12-11 18:24:33 +0100

    add docker compose auth service

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,6 +6,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "80"
@@ -19,9 +25,16 @@ func main() {
 	log.Println("Starting authentication service")
 
 	// connect to db
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres!")
+	}
 
 	// setup config
-	app := Config{}
+	app := Config{
+		DB: conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%s", webPort),
@@ -31,5 +44,44 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	var counts int64
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready...")
+			counts++
+		} else {
+			log.Println("Connected to Postgres!")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+		log.Println("Backing off for 2 seconds...")
+		time.Sleep(2 * time.Second)
+		continue
 	}
 }
--- authentication-service/go.mod
+++ authentication-service/go.mod
@@ -10,4 +10,15 @@ require (
 require (
 	github.com/go-chi/chi v1.5.5 // indirect
 	github.com/go-chi/cors v1.2.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v4 v4.18.3 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/text v0.32.0 // indirect
 )
